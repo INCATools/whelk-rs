b1594f55c8bf69e810550144d567236ab04c2012
Jason Reilly <jdr0887@gmail.com>
2023-03-07 12:19:24 -0500

    removing pub modifier

--- src/whelk/reasoner.rs
+++ src/whelk/reasoner.rs
@@ -16,7 +16,7 @@ pub struct ReasonerState {
     inits: HashSet<Rc<Concept>>,
     asserted_concept_inclusions_by_subclass: HashMap<Rc<Concept>, Vector<Rc<ConceptInclusion>>>,
     closure_subs_by_superclass: HashMap<Rc<Concept>, HashSet<Rc<Concept>>>,
-    pub closure_subs_by_subclass: HashMap<Rc<Concept>, HashSet<Rc<Concept>>>,
+    closure_subs_by_subclass: HashMap<Rc<Concept>, HashSet<Rc<Concept>>>,
     asserted_negative_conjunctions: HashSet<Rc<Conjunction>>,
     asserted_negative_conjunctions_by_right_operand: HashMap<Rc<Concept>, HashMap<Rc<Concept>, Rc<Conjunction>>>,
     asserted_negative_conjunctions_by_left_operand: HashMap<Rc<Concept>, HashMap<Rc<Concept>, Rc<Conjunction>>>,
@@ -699,4 +699,142 @@ fn index_role_compositions(hier: &HashMap<Rc<Role>, HashSet<Rc<Role>>>, chains: 
         }
     }
     hier_comps
+}
+
+#[cfg(test)]
+mod test {
+    use crate::read_input;
+    use crate::whelk::model as wm;
+    use crate::whelk::owl::translate_ontology;
+    use crate::whelk::reasoner::assert;
+    use horned_owl::model::RcStr;
+    use horned_owl::ontology::set::SetOntology;
+    use im::{HashMap, HashSet};
+    use std::ops::Deref;
+    use std::rc::Rc;
+    use std::{error, fs, path};
+
+    fn load_test_ontologies(parent_path: &path::PathBuf) -> Result<(Option<SetOntology<RcStr>>, Option<SetOntology<RcStr>>, Option<SetOntology<RcStr>>), Box<dyn error::Error>> {
+        let parent_name = parent_path.file_name().unwrap();
+        let asserted_path = parent_path.clone().join(format!("{}-asserted.owx", parent_name.to_string_lossy()));
+        let entailed_path = parent_path.clone().join(format!("{}-entailed.owx", parent_name.to_string_lossy()));
+        let invalid_path = parent_path.clone().join(format!("{}-invalid.owx", parent_name.to_string_lossy()));
+
+        let asserted_ontology = read_input(&asserted_path).expect("failed to read asserted ontology file");
+
+        let ret = match (entailed_path.exists(), invalid_path.exists()) {
+            (true, true) => {
+                let entailed_ontology = read_input(&entailed_path).expect("failed to read entailed ontology file");
+                let invalid_ontology = read_input(&invalid_path).expect("failed to read invalid ontology file");
+                (Some(asserted_ontology), Some(entailed_ontology), Some(invalid_ontology))
+            }
+            (true, false) => {
+                let entailed_ontology = read_input(&entailed_path).expect("failed to read entailed ontology file");
+                (Some(asserted_ontology), Some(entailed_ontology), None)
+            }
+            (false, true) => {
+                let invalid_ontology = read_input(&invalid_path).expect("failed to read invalid ontology file");
+                (Some(asserted_ontology), None, Some(invalid_ontology))
+            }
+            _ => (None, None, None),
+        };
+
+        Ok(ret)
+    }
+
+    #[test]
+    fn test_for_subclassof() {
+        let data_inference_tests_dir = path::PathBuf::from("./src/data/inference-tests");
+        let read_dir_results = fs::read_dir(data_inference_tests_dir).expect("no such directory?");
+
+        let assert_entailed_whelk_axioms_exist_in_map =
+            |whelk_subs_by_subclass: &HashMap<Rc<wm::Concept>, HashSet<Rc<wm::Concept>>>, whelk_axioms: &HashSet<Rc<wm::Axiom>>| -> () {
+                whelk_axioms.iter().map(|a| Rc::deref(a)).for_each(|a| match a {
+                    wm::Axiom::ConceptInclusion(ci) => match (Rc::deref(&ci.subclass), Rc::deref(&ci.superclass)) {
+                        (wm::Concept::AtomicConcept(sub), wm::Concept::AtomicConcept(sup)) => {
+                            let subclass_deref = ci.subclass.deref();
+                            let supclass_deref = ci.superclass.deref();
+                            assert!(whelk_subs_by_subclass.contains_key(subclass_deref));
+                            assert!(whelk_subs_by_subclass.contains_key(supclass_deref));
+                            let values_by_subclass = whelk_subs_by_subclass.get(subclass_deref);
+                            assert!(values_by_subclass.is_some(), "{}", format!("sub class key is not found: {:?}", subclass_deref));
+                            assert!(
+                                values_by_subclass.unwrap().contains(supclass_deref),
+                                "{}",
+                                format!("super class is not contained in sub class set of values: {:?}:{:?}", subclass_deref, supclass_deref)
+                            );
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                });
+            };
+
+        let assert_invalid_whelk_axioms_exist_in_map = |whelk_subs_by_subclass: &HashMap<Rc<wm::Concept>, HashSet<Rc<wm::Concept>>>, whelk_axioms: &HashSet<Rc<wm::Axiom>>| -> () {
+            whelk_axioms.iter().map(|a| Rc::deref(a)).for_each(|a| match a {
+                wm::Axiom::ConceptInclusion(ci) => match (Rc::deref(&ci.subclass), Rc::deref(&ci.superclass)) {
+                    (wm::Concept::AtomicConcept(sub), wm::Concept::AtomicConcept(sup)) => {
+                        let subclass_deref = ci.subclass.deref();
+                        let supclass_deref = ci.superclass.deref();
+                        assert!(!whelk_subs_by_subclass.contains_key(subclass_deref));
+                        assert!(!whelk_subs_by_subclass.contains_key(supclass_deref));
+                    }
+                    _ => {}
+                },
+                _ => {}
+            });
+        };
+
+        let test_directories: Vec<path::PathBuf> = read_dir_results
+            .flat_map(|a| a.map(|b| b.path()))
+            .filter_map(|a| {
+                let path = a.as_path();
+                if path.is_dir() {
+                    Some(path.to_path_buf())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        test_directories.iter().for_each(|test_dir| {
+            println!("testing directory: {:?}", test_dir);
+            let (asserted_ontology, entailed_ontology, invalid_ontology) = load_test_ontologies(&test_dir).expect("could not get test ontologies");
+
+            match (asserted_ontology, entailed_ontology, invalid_ontology) {
+                (Some(ao), Some(eo), Some(io)) => {
+                    let asserted_whelk_axioms = translate_ontology(&ao);
+
+                    let whelk = assert(&asserted_whelk_axioms);
+                    let whelk_subs_by_subclass = whelk.closure_subs_by_subclass;
+                    // whelk_subs_by_subclass.iter().for_each(|a| println!("subclass: {:?}", a));
+
+                    let entailed_whelk_axioms = translate_ontology(&eo);
+                    assert_entailed_whelk_axioms_exist_in_map(&whelk_subs_by_subclass, &entailed_whelk_axioms);
+
+                    let invalid_whelk_axioms = translate_ontology(&io);
+                    assert_invalid_whelk_axioms_exist_in_map(&whelk_subs_by_subclass, &invalid_whelk_axioms);
+                }
+                (Some(ao), Some(eo), None) => {
+                    let asserted_whelk_axioms = translate_ontology(&ao);
+
+                    let whelk = assert(&asserted_whelk_axioms);
+                    let whelk_subs_by_subclass = whelk.closure_subs_by_subclass;
+
+                    let entailed_whelk_axioms = translate_ontology(&eo);
+                    assert_entailed_whelk_axioms_exist_in_map(&whelk_subs_by_subclass, &entailed_whelk_axioms);
+                }
+                (Some(ao), None, Some(io)) => {
+                    let asserted_whelk_axioms = translate_ontology(&ao);
+
+                    let whelk = assert(&asserted_whelk_axioms);
+                    let whelk_subs_by_subclass = whelk.closure_subs_by_subclass;
+
+                    let invalid_whelk_axioms = translate_ontology(&io);
+                    assert_invalid_whelk_axioms_exist_in_map(&whelk_subs_by_subclass, &invalid_whelk_axioms);
+                }
+                _ => {}
+            }
+        });
+    }
 }
